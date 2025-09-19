45b0c832f25b7fd200f11b84cc16041f8cb3dc47
Rohith Edwin <rohithedwin28193@gmail.com>
2025-09-19 19:58:21 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="AIHealthCareBot",
+    version="0.1.0",
+    author="Code Hex",
+    packages=find_packages(),
+    install_requires=[],
+)
